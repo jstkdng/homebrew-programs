4f507061dc79226fbd19380ae55a973587f839e2
JustKidding <jk@vin.ovh>
2024-11-26 11:36:08 -0500

    update to 2.9.6

--- ueberzugpp.rb
+++ ueberzugpp.rb
@@ -3,27 +3,23 @@ require "pty"
 class Ueberzugpp < Formula
   desc "Drop in replacement for ueberzug written in C++"
   homepage "https://github.com/jstkdng/ueberzugpp"
-  url "https://github.com/jstkdng/ueberzugpp/archive/refs/tags/v2.9.2.tar.gz"
-  sha256 "a658ccdb82c50ebd3bf31ecaaa79bcbb66bae756c3b588d4208c94a17f127f42"
+  url "https://github.com/jstkdng/ueberzugpp/archive/refs/tags/v2.9.6.tar.gz"
+  sha256 "29cca04404c5883510aebf02846f608cfbf5892176bf4a48099e5167d5ef9d95"
   head "https://github.com/jstkdng/ueberzugpp.git"
   license "GPL-3.0-or-later"
 
   depends_on "cli11" => :build
   depends_on "cmake" => :build
-  depends_on "cpp-gsl" => :build
   depends_on "nlohmann-json" => :build
   depends_on "pkg-config" => :build
+  depends_on "range-v3" => :build
   depends_on "chafa"
   depends_on "fmt"
   depends_on "libsixel"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "spdlog"
   depends_on "tbb"
   depends_on "vips"
-
-  on_macos do
-    depends_on "range-v3" => :build
-  end
 
   on_linux do
     depends_on "extra-cmake-modules" => :build
